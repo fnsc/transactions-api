41c959da05b6e37443ccfdba246912f830a6ef81
Gabriel Fonseca <gdfonseca@leroymerlin.com.br>
2022-06-11 10:47:08 -0300

    chore(packages): upgrade laravel

--- rector.php
+++ rector.php
@@ -2,24 +2,28 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
+use Rector\Config\RectorConfig;
+use Rector\Laravel\Set\LaravelSetList;
 use Rector\Php74\Rector\Property\TypedPropertyRector;
 use Rector\Set\ValueObject\LevelSetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    // get parameters
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PATHS, [
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
         __DIR__ . '/app',
         __DIR__ . '/src',
+        __DIR__ . '/tests',
     ]);
 
     // Define what rule sets will be applied
-    $containerConfigurator->import(LevelSetList::UP_TO_PHP_81);
+    $rectorConfig->import(LevelSetList::UP_TO_PHP_81);
+
+    // Laravel 9
+    $rectorConfig->sets([
+        LaravelSetList::LARAVEL_90,
+    ]);
 
     // get services (needed for register a single rule)
-     $services = $containerConfigurator->services();
+     $services = $rectorConfig->services();
 
     // register a single rule
      $services->set(TypedPropertyRector::class);
